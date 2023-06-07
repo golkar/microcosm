6bcc00b08e746e055132e72f5db3fc469dc8c876
Siavash Golkar <sgolkar@flatironinstitute.org>
2023-06-07 17:32:31 -0400

    Added KCroberta first results

--- KCroberta_xslorenz_sweep_setup.py
+++ KCroberta_xslorenz_sweep_setup.py
@@ -14,16 +14,16 @@ parameters_dict = {
     "max_position_embeddings": {"value": 1150},
     "hidden_size": {"value": 720},  # needs to be divisible by num_attention_heads
     "num_hidden_layers": {"value": 12},
-    "num_attention_heads": {"value": 6},
+    "num_attention_heads": {"value": 8},
     "hidden_act": {"value": "gelu"},
     "hidden_dropout_prob": {"value": 0},
     "attention_probs_dropout_prob": {"value": 0},
     "mlm_probability": {"value": 0.2},
-    "num_train_epochs": {"value": 5},
-    "warmup_steps": {"value": 2000},
+    "num_train_epochs": {"value": 20},
+    "warmup_steps": {"value": 5000},
     "weight_decay": {"value": 0.0001},
     "fp16": {"value": True},
-    "power_num": {"value": 1 / 3},
+    "power_num": {"value": 1 / 2},
 }
 
 parameters_dict["intermediate_size"] = {
@@ -46,5 +46,7 @@ sweep_id = wandb.sweep(sweep_config, project="xslorenz_kcroberta")
 # new sweep with torch save at the end
 # sweep_id = "wqeq85eb" grid [2*1.5**n * 1e-5 for n in range(10)]
 
+# width 720 sweeps, power_num 1/2, 20 epochs
+# sweep_id = "cz111509" grid [2*1.5**n * 1e-5 for n in range(10)]
 
 # %%
